0e91559f0a870dc6a4712ec625ee14f3cb9f9c29
Terry Fallows <ianterryfallows@gmail.com>
2022-06-07 16:32:35 +0100

    Heroku App Creation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const server = require('./server')
+const server = require('./server');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-server.listen(port, () => console.log(`\nExpress departing now from port 3000!\n`))
+server.listen(port, () => console.log(`\nExpress departing now from port 3000!\n`));
